7ab6fe6a27c75e73bcda93fc1dc48e6be520ca5e
yamil rosa <yamilrosa@live.com>
2021-01-21 23:03:52 -0500

    added Rating component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,13 +4,12 @@ import Product from "../components/Product";
 import products from "../products.js";
 
 const HomeScreen = () => {
-  console.log(products);
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
         {products.map(product => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
