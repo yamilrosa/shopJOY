7d3b34d540651384ce8c14285b929256256fba07
yamil rosa <yamilrosa@live.com>
2021-01-25 15:58:16 -0500

    added routes file and setup product routes with express

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -71,6 +71,6 @@ const orderSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-const Order = mongoose.model("Orderr", orderSchema);
+const Order = mongoose.model("Order", orderSchema);
 
 export default Order;
